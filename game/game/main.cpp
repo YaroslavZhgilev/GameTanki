715c7968fa7ae877620e46ff249a932fb77ee5b5
Yaroslav Zhgilev <jarslavzhgilev@rambler.ru>
2019-12-14 00:04:06 +0300

    Add map

--- game/game/main.cpp
+++ game/game/main.cpp
@@ -1,13 +1,72 @@
 #include <SFML/Graphics.hpp> 
 #include <iostream>
 #include <cmath>
+#include "map.h" //подключили код с картой 
  
 using namespace sf;//включаем пространство имен sf, чтобы постоянно не писать 
+
+////////////////////////////////////////////////////КЛАСС ИГРОКА//////////////////////// 
+class Player { // класс Игрока 
+public:  float x, y, w, h, dx, dy, speed; //координаты игрока х и у, высота и ширина,        
+		 //ускорение (по х и по у), сама скорость  
+		 int dir; //направление (direction) движения игрока  
+		 std::string File; //файл с расширением  
+		 Image image;//сфмл изображение  
+		 Texture texture;//сфмл текстура  
+		 Sprite sprite;//сфмл спрайт 
+		 //Конструктор с параметрами для класса Player. При создании объекта класса мы будем задавать 
+		 //имя файла, координату Х и У, ширину и высоту 
+		 Player(std::string F, float X, float Y, float W, float H){
+			 File = F; //имя файла+расширение  
+			 w = W; 
+			 h = H; //высота и ширина  
+			 image.loadFromFile("images/" + File);//загружаем в image изображение, вместо File  
+			 //передадим то, что пропишем при создании объекта. В нашем случае это "hero.png". Получится  
+			 //запись, идентичная image.loadFromFile("images/hero/png");  
+			 image.createMaskFromColor(Color(41, 33, 59)); //убираем ненужный темно-синий цвет 
+			 texture.loadFromImage(image); //заносим наше изображение в текстуру  
+			 sprite.setTexture(texture); //заливаем спрайт текстурой  
+			 x = X; 
+			 y = Y; //координата появления спрайта        
+			 speed= 0;        
+			 dir= 0;  
+			 sprite.setTextureRect(IntRect(0, 0, w, h)); //Задаем спрайту один прямоугольник для  //вывода одного льва. IntRect – для приведения типов 
+}
+		 void update(float time) //функция "оживления/обновления" объекта класса. Принимает в себя   
+			 //время SFML, вследствие чего работает бесконечно, давая персонажу движение. 
+		 {  
+			 switch (dir)//реализуем поведение в зависимости от направления. Каждая цифра 
+				 //соответствует направлению.  
+			 {
+				case 0: dx = speed; dy = 0; break; //по “X” задаем положительную скорость, по “Y” - 0.         
+					//Персонаж идет только вправо.  
+				case 1: dx = -speed; dy = 0; break;//по “X” задаем отрицательную скорость, по и “Y” – 0.      
+					//Персонаж идет только влево.  
+				case 2: dx = 0; dy = speed; break;//по “X” задаем нулевое значение, по “Y”     
+					//положительное. Получается, что персонаж идет только вниз  
+				case 3: dx = 0; dy = -speed; break;//по “X” задаем нулевое значение, по “Y”    
+					//отрицательное. Получается, что персонаж идет только вверх  
+			 } 
+ 
+		 x += dx*time; //движение по “X”  
+		 y += dy*time; //движение по “Y”  
+		 speed = 0;    //обнуляем скорость, чтобы персонаж остановился. 
+		 sprite.setPosition(x, y); //выводим спрайт в позицию (x, y).     //бесконечно выводим в этой функции, иначе бы наш спрайт стоял на месте. 
+		 } 
+};
 int main() {  //Создаём окно   
-	
+	Player p("hero.png", 250, 250, 96.0, 96.0); //создаем объект “p” класса “Player”, //задаем "hero.png" как имя файла+расширение, далее координата Х,У, ширина, высота.
+
 	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();  
 	sf::RenderWindow window(sf::VideoMode(640, 480, desktop.bitsPerPixel), "Lesson 6"); 
- 
+
+	Image map_image;//объект изображения для карты 
+	map_image.loadFromFile("images/map.png");//загружаем файл для карты 
+	Texture map;//текстура карты 
+	map.loadFromImage(map_image);//заряжаем текстуру картинкой  
+	Sprite s_map;//создаём спрайт для карты 
+	s_map.setTexture(map);//заливаем текстуру спрайтом 
+
 	Image heroimage; //создаем объект Image (изображение)  
 	heroimage.loadFromFile("images/hero.png");//загружаем в него файл 
  
@@ -42,43 +101,61 @@ int main() {  //Создаём окно
  
 		 if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) 
 		 { 
-			 //если нажата клавиша стрелка влево или англ буква А  
-			 CurrentFrame += 0.005*time;  //служит для прохождения по "кадрам". Переменная доходит до трех, суммируя произведение 
-			 //времени и скорости. Изменив 0.005, можно изменить скорость анимации  
-			 if (CurrentFrame > 3) CurrentFrame -= 3; //если пришли к третьему кадру - откатываемся 
-			 //назад.  
-			 herosprite.setTextureRect(IntRect(96 * int(CurrentFrame), 96, 96, 96)); //проходим по 
-			 //координатам Х. Получается, что начинаем рисование с координаты Х равной 0,96,96*2, и опять 0  
-			 herosprite.move(-0.1*time, 0); //происходит движение персонажа влево 
+			 p.dir = 1; p.speed = 0.1;//dir =1 - направление вверх, speed =0.1 - скорость движения.   
+			 //Заметьте - время мы уже здесь ни на что не умножаем и нигде не используем каждый раз  
+			 CurrentFrame += 0.005*time;  
+			 if (CurrentFrame > 3) CurrentFrame -= 3;  
+			 p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 96, 96, 96)); //через объект p         
+			 //класса player меняем спрайт, делая анимацию  
 		 } 
 
 		 if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))) 
 		 {   
-			 CurrentFrame += 0.005*time;     
-			 if (CurrentFrame > 3) CurrentFrame -= 3;     
-			 herosprite.setTextureRect(IntRect(96 * int(CurrentFrame), 192, 96, 96));     //проходим по координатам Х. получается 96,96*2,96*3 и опять 96 
-		     herosprite.move(0.1*time, 0); //движение персонажа вправо 
+			 p.dir = 0; p.speed = 0.1;//направление вправо  
+			 CurrentFrame += 0.005*time;  
+			 if (CurrentFrame > 3) CurrentFrame -= 3; 
+			 p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 192, 96, 96)); //через объект p        
+			 //класса player меняем спрайт, делая анимацию
  
 		 }
 
 		 if ((Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)))) 
 		 {    
-			 CurrentFrame += 0.005*time;     
-			 if (CurrentFrame > 3) CurrentFrame -= 3;     
-			 herosprite.setTextureRect(IntRect(96 * int(CurrentFrame), 288, 96, 96));     //проходим по координатам Х. получается 96,96*2,96*3 и опять 96   
-			 herosprite.move(0, -0.1*time); //движение персонажа вверх   
+			 p.dir = 3; p.speed = 0.1;//направление вниз  
+			 CurrentFrame += 0.005*time;  
+			 if (CurrentFrame > 3) CurrentFrame -= 3; 
+			 p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 288, 96, 96)); //через объект p    
+			 //класса player меняем спрайт, делая анимацию (используя оператор точку)   
 		 } 
 
          if ((Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)))) 
 		 {    
-			 CurrentFrame += 0.005*time;     
-			 if (CurrentFrame > 3) CurrentFrame -= 3;     
-			 herosprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));     //проходим по координатам Х. получается 96,96*2,96*3 и опять 96    
-			 herosprite.move(0, 0.1*time); // движение персонажа вниз   
+			 p.dir = 2; p.speed = 0.1;//направление вверх 
+			 CurrentFrame += 0.005*time; 
+			 if (CurrentFrame > 3) CurrentFrame -= 3; 
+			 p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));    
 		 }
 
+		 p.update(time); //оживляем объект “p” класса “Player” с помощью времени sfml, 
+		 // передавая время в качестве параметра функции update. 
   window.clear(); //Очищаем экран   
-  window.draw(herosprite);//рисуем объект   
+
+  /////////////////////////////Рисуем карту///////////////////// 
+  for (int i = 0; i < HEIGHT_MAP; i++) 
+	  for (int j = 0; j < WIDTH_MAP; j++)  {  
+		  if (TileMap[i][j] == ' ')  s_map.setTextureRect(IntRect(0, 0, 32, 32)); //если        
+		  //встретили символ пробел, то рисуем 1-й квадратик   
+		  if (TileMap[i][j] == 's')  s_map.setTextureRect(IntRect(32, 0, 32, 32));//если   
+		  //встретили символ s, то рисуем 2й квадратик   
+		  if ((TileMap[i][j] == '0')) s_map.setTextureRect(IntRect(64, 0, 32, 32));//если 
+		  //встретили символ 0, то рисуем 3й квадратик 
+ 
+  s_map.setPosition(j * 32, i * 32);//раскладываем квадратики в карту. 
+ 
+  window.draw(s_map);//рисуем квадратики на экран 
+	  }
+
+  window.draw(p.sprite);//рисуем объект   
   window.display(); //Показываем объект на экране 
 	 } 
  
