eed3f7303e420e17f787c575b7df44b7261c7991
Yaroslav Zhgilev <jarslavzhgilev@rambler.ru>
2019-12-14 20:39:58 +0300

    Izmenili karty, dobavili psevdorand, izmenili upravlenie

--- game/game/main.cpp
+++ game/game/main.cpp
@@ -7,19 +7,26 @@
 using namespace sf;//включаем пространство имен sf, чтобы постоянно не писать 
 
 ////////////////////////////////////////////////////КЛАСС ИГРОКА//////////////////////// 
-class Player { // класс Игрока 
-public:  float x, y, w, h, dx, dy, speed; //координаты игрока х и у, высота и ширина,        
+class Player { // класс Игрока
+private: float x,y;
+public:  float w, h, dx, dy, speed; //координаты игрока х и у, высота и ширина,        
 		 //ускорение (по х и по у), сама скорость  
-		 int playerScore;//новая переменная, хранящая очки игрока 
-		 int dir; //направление (direction) движения игрока  
+		 float CurrentFrame;//хранит текущий кадр 
+		 int playerScore,health;//новая переменная, хранящая очки игр //направление (direction) движения игрока
+		 bool life;
+		 enum {left, right, up, down, stay} state;//добавляем тип перечисления - состояние объекта
 		 std::string File; //файл с расширением  
 		 Image image;//сфмл изображение  
 		 Texture texture;//сфмл текстура  
 		 Sprite sprite;//сфмл спрайт 
 		 //Конструктор с параметрами для класса Player. При создании объекта класса мы будем задавать 
 		 //имя файла, координату Х и У, ширину и высоту 
+
 		 Player(std::string F, float X, float Y, float W, float H){
-			 dir = 0; speed = 0; playerScore = 0; 
+			 dx=0; dy=0; speed = 0; playerScore = 0;health=100;
+			 CurrentFrame=0;
+			 state=stay;
+			 life=true;
 			 File = F; //имя файла+расширение  
 			 w = W; 
 			 h = H; //высота и ширина  
@@ -30,73 +37,120 @@ public:  float x, y, w, h, dx, dy, speed; //координаты игрока х и у, высота и ши
 			 texture.loadFromImage(image); //заносим наше изображение в текстуру  
 			 sprite.setTexture(texture); //заливаем спрайт текстурой  
 			 x = X; 
-			 y = Y; //координата появления спрайта        
-			 speed= 0;        
-			 dir= 0;  
+			 y = Y; //координата появления спрайта                  
 			 sprite.setTextureRect(IntRect(0, 0, w, h)); //Задаем спрайту один прямоугольник для  //вывода одного льва. IntRect – для приведения типов 
+}
+
+		 void control(){    
+				if (Keyboard::isKeyPressed(Keyboard::Left)) {     
+					state = left;   
+					speed = 0.1;   
+				}    
+				if (Keyboard::isKeyPressed(Keyboard::Right)) {     
+					state = right;  
+					speed = 0.1; 
+				} 
+ 
+			    if (Keyboard::isKeyPressed(Keyboard::Up)) {    
+					state = up; 
+					speed = 0.1; 
+				} 
+ 
+			    if (Keyboard::isKeyPressed(Keyboard::Down)) { 
+					state = down;   
+				    speed = 0.1;  
+				}  
 }
 		 void update(float time) //функция "оживления/обновления" объекта класса. Принимает в себя   
 			 //время SFML, вследствие чего работает бесконечно, давая персонажу движение. 
 		 {  
-			 switch (dir)//реализуем поведение в зависимости от направления. Каждая цифра 
+			 if(life){//жив ли герой
+				 control();//функция управления персонажем
+			 switch (state)//реализуем поведение в зависимости от направления. Каждая цифра 
 				 //соответствует направлению.  
 			 {
-				case 0: dx = speed; dy = 0; break; //по “X” задаем положительную скорость, по “Y” - 0.         
-					//Персонаж идет только вправо.  
-				case 1: dx = -speed; dy = 0; break;//по “X” задаем отрицательную скорость, по и “Y” – 0.      
-					//Персонаж идет только влево.  
-				case 2: dx = 0; dy = speed; break;//по “X” задаем нулевое значение, по “Y”     
-					//положительное. Получается, что персонаж идет только вниз  
-				case 3: dx = 0; dy = -speed; break;//по “X” задаем нулевое значение, по “Y”    
-					//отрицательное. Получается, что персонаж идет только вверх  
-			 } 
+				case right:{//состояние идти вправо    
+					dx = speed;    
+					CurrentFrame += 0.005*time;   
+					if (CurrentFrame > 3) CurrentFrame -= 3;
+					sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 192, 96, 96)); 
+				    break; 
+						  }  
+				case left:{//состояние идти влево   
+					dx = -speed;  
+					CurrentFrame += 0.005*time;   
+					if (CurrentFrame > 3) CurrentFrame -= 3;   
+					sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 96, 96, 96)); 
+					break;   
+						  }
+
+				case up:{//идти вверх   
+					dy = -speed;  
+					CurrentFrame += 0.005*time;    
+					if (CurrentFrame > 3) CurrentFrame -= 3; 
+					sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 288, 96, 96)); 
+					break; 
+						}
+
+				case down:{//идти вниз 
+					dy = speed;   
+					CurrentFrame += 0.005*time;  
+					if (CurrentFrame > 3) CurrentFrame -= 3;    
+					sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));
+					break; 
+						  }   
+				case stay:{//стоим    
+					dy = speed;   
+					dx = speed;    
+				    break;     
+						  } 
+			 }
  
-		 x += dx*time; //движение по “X”  
-		 y += dy*time; //движение по “Y”  
-		 speed = 0;    //обнуляем скорость, чтобы персонаж остановился. 
-		 sprite.setPosition(x, y); //выводим спрайт в позицию (x, y).     
-		 //бесконечно выводим в этой функции, иначе бы наш спрайт стоял на месте.
-		 interactionWithMap();//вызываем функцию, отвечающую за взаимодействие с картой 
+		 x += dx*time; //движение по “X”   
+		 checkCollisionWithMap(dx, 0);//обрабатываем столкновение по Х 
+	     y += dy*time; //движение по “Y” 
+		 checkCollisionWithMap(0, dy);//обрабатываем столкновение по Y 
+		 speed = 0;    //обнуляем скорость, чтобы персонаж остановился.   //state = stay; //состояние - стоит 
+	     sprite.setPosition(x, y); //переставляем спрайт в новую позицию (x, y). 
+ 
+		if (health <= 0){ life = false; }//если жизней меньше 0, либо равно 0, то умираем 
+			 } 
 		 }
 
-		 void interactionWithMap()//ф-ция взаимодействия с картой 
-		 {
-			 //Проходим только по тем тайлам (квадратикам размера 32*32), которые контактируют с игроком. //Частично или полностью находятся под изображением игрока!   
-			 for (int i = y / 32; i < (y + h) / 32; i++)   
-				 for (int j = x / 32; j<(x + w) / 32; j++){ 
-					 //”x” делим на 32, тем самым получаем левый квадратик, с которым персонаж соприкасается. 
-					 //Он ведь больше размера 32*32, поэтому может одновременно стоять на нескольких тайлах 
-					 //Кроме того, j<(x + w)/32 - условие ограничения координат по “x”, т.е. координата самого 
-					 //правого квадрата, который соприкасается с персонажем. таким образом идем в цикле слева 
-					 // направо по иксу, проходя от левого квадрата (соприкасающегося с героем), до правого 
-					 // квадрата (соприкасающегося с героем)   
-					 if (TileMap[i][j] == '0')//если наш квадратик соответствует символу “0”      
-						 //(стена), то проверяем "направление скорости" персонажа:  
-					 {   
-						 if (dy>0) {
-							 //если мы шли вниз,    
-							 y = i * 32 - h;//то стопорим (-h) координату “y” персонажа.      
-							 //сначала получаем координату “i” нашего квадратика на карте и     
-							 //затем вычитаем из высоты спрайта персонажа.    
-						 }    
-						 if (dy<0){    
-							 y = i * 32 + 32;//аналогично с движением вверх. 
-						 }    
-						 if (dx>0){    
-							 x = j * 32 - w;//если идем вправо, то координата “x” равна         
-							 //стена (символ 0) минус ширина персонажа    
-						 }    
-						 if (dx < 0){   
-							 x = j * 32 + 32; //аналогично идем влево    
-						 }   
-					 }     
+	//Функция проверки столкновений с элементами карты 
+		 void checkCollisionWithMap(float Dx, float Dy) {
+			 for (int i = y / 32; i < (y + h) / 32; i++)//проходимся по элементам карты 
+				 for (int j = x / 32; j<(x + w) / 32; j++)  { 
+					 if (TileMap[i][j] == '0')//если элемент тайлик земли   
+					 { 
+						 if (Dy > 0) { y = i * 32 - h;  dy = 0; }//по Y   
+						 if (Dy < 0) { y = i * 32 + 32; dy = 0; }//столкновение с верхними краями карты  
+						 if (Dx > 0) { x = j * 32 - w; dx = 0; }//с правым краем карты   
+						 if (Dx < 0) { x = j * 32 + 32; dx = 0; }// с левым краем карты 
+					 }   
 					 if (TileMap[i][j] == 's') { 
-						 //если символ равен 's' (камень)     
-						 playerScore++;//какое-то действие     
-						 TileMap[i][j] = ' ';//убираем камень    
-					 } 
+						 playerScore++; //если взяли камень, переменная playerScore=playerScore+1; 
+						 TileMap[i][j] = ' ';  
+							} 
+					 if (TileMap[i][j] == 'f') {  
+						 health -= 40;//если взяли ядовитый цветок,то переменная health=health-40;    
+						 TileMap[i][j] = ' ';//убрали цветок  
+							} 
+					 if (TileMap[i][j] == 'h') {  
+						 health += 20;//если взяли сердечко,то переменная health=health+20; 
+						 TileMap[i][j] = ' ';//убрали сердечко  
+					 }
 				 }
-}
+		 }
+
+		 FloatRect getRect(){
+			 //метод получения прямоугольника. его коорд, размеры (шир,высот). 
+			 FloatRect FR(x, y, w, h); // переменная FR типа FloatRect  
+			 return FR;  //Тип данных (класс) "sf::FloatRect" позволяет хранить четыре координаты прямоугольника 
+			 //в нашей игре это координаты текущего расположения тайла на карте 
+			 //далее это позволит спросить, есть ли ещё какой-либо тайл на этом месте   
+			 //эта ф-ция нужна для проверки пересечений   
+		 }
 };
 int main() {  //Создаём окно   
 	Player p("hero.png", 250, 250, 96.0, 96.0); //создаем объект “p” класса “Player”, //задаем "hero.png" как имя файла+расширение, далее координата Х,У, ширина, высота.
@@ -112,34 +166,35 @@ Text text("", font, 20);//создаем объект текст. закидываем в объект текст строку
 text.setColor(Color::Red);//покрасили текст в красный. если убрать эту строку, то по умолчанию он белый  
 text.setStyle(Text::Bold);//жирный текст.
 	Image map_image;//объект изображения для карты 
-	map_image.loadFromFile("images/map.png");//загружаем файл для карты 
+	map_image.loadFromFile("images/map_new.png");//загружаем файл для карты 
 	Texture map;//текстура карты 
 	map.loadFromImage(map_image);//заряжаем текстуру картинкой  
 	Sprite s_map;//создаём спрайт для карты 
-	s_map.setTexture(map);//заливаем текстуру спрайтом 
-
-	Image heroimage; //создаем объект Image (изображение)  
-	heroimage.loadFromFile("images/hero.png");//загружаем в него файл 
+	s_map.setTexture(map);//заливаем текстуру спрайтом   
  
-	Texture herotexture;//создаем объект Texture (текстура)  
-	herotexture.loadFromImage(heroimage);//передаем в него объект Image (изображения) 
-
-	float CurrentFrame=0; //хранит текущий кадр 
 	Clock clock;   //создаем переменную времени и одновременно запускаем часы!
+	Clock gameTimeClock;//переменная игрового времени, будем здесь хранить время игры  
+	int gameTime = 0;//объявили игровое время, инициализировали.
  
-	 Sprite herosprite;//создаем объект Sprite(спрайт)  
-	 herosprite.setTexture(herotexture);//передаём в него объект Texture (текстуры)  
-	 herosprite.setTextureRect(IntRect(0, 192, 96, 96));//получили нужный прямоугольник 
-	 herosprite.setPosition(250, 250);//задаем начальные координаты появления спрайта 
- 
+	int createObjectForMapTimer = 0;//Переменная под время для генерирования камней 
+
  while (window.isOpen())  //Пока окно открыто  
 	{   
 
 		// дать время с последнего перезапуска часов, в данном случае время, прошедшее с              
 		//предыдущей итерации и вызова restart();   
-		float time = clock.getElapsedTime().asMicroseconds();    
+		float time = clock.getElapsedTime().asMicroseconds();
+		if (p.life) gameTime = gameTimeClock.getElapsedTime().asSeconds();   
+		//игровое время в секундах идёт вперед, пока жив игрок.     
+		//Перезагружать как time его не надо. оно не обновляет логику игры
 		clock.restart(); //перезапуск часов   
-		time = time / 800; //скорость игры   
+		time = time / 800; //скорость игры  
+
+		createObjectForMapTimer += time;//наращиваем таймер   
+		if (createObjectForMapTimer>100){    
+			randomMapGenerate();//генерация  камней   
+			createObjectForMapTimer = 0;//обнуляем таймер   
+ }
 		    
 		 sf::Event event;   
 		 while (window.pollEvent(event))  
@@ -147,45 +202,6 @@ text.setStyle(Text::Bold);//жирный текст.
 				 if (event.type == sf::Event::Closed)    
 				 window.close();   
 			} 
-		 //"-0,1" - будем считать скоростью, умножаем её на наше время “time” и получаем    
-		 //пройденное расстояние 
- 
-		 if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) 
-		 { 
-			 p.dir = 1; p.speed = 0.1;//dir =1 - направление вверх, speed =0.1 - скорость движения.   
-			 //Заметьте - время мы уже здесь ни на что не умножаем и нигде не используем каждый раз  
-			 CurrentFrame += 0.005*time;  
-			 if (CurrentFrame > 3) CurrentFrame -= 3;  
-			 p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 96, 96, 96)); //через объект p         
-			 //класса player меняем спрайт, делая анимацию  
-		 } 
-
-		 if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))) 
-		 {   
-			 p.dir = 0; p.speed = 0.1;//направление вправо  
-			 CurrentFrame += 0.005*time;  
-			 if (CurrentFrame > 3) CurrentFrame -= 3; 
-			 p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 192, 96, 96)); //через объект p        
-			 //класса player меняем спрайт, делая анимацию
- 
-		 }
-
-		 if ((Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)))) 
-		 {    
-			 p.dir = 3; p.speed = 0.1;//направление вниз  
-			 CurrentFrame += 0.005*time;  
-			 if (CurrentFrame > 3) CurrentFrame -= 3; 
-			 p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 288, 96, 96)); //через объект p    
-			 //класса player меняем спрайт, делая анимацию (используя оператор точку)   
-		 } 
-
-         if ((Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)))) 
-		 {    
-			 p.dir = 2; p.speed = 0.1;//направление вверх 
-			 CurrentFrame += 0.005*time; 
-			 if (CurrentFrame > 3) CurrentFrame -= 3; 
-			 p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));    
-		 }
 
 		 p.update(time); //оживляем объект “p” класса “Player” с помощью времени sfml, 
 		 // передавая время в качестве параметра функции update. 
@@ -200,17 +216,18 @@ text.setStyle(Text::Bold);//жирный текст.
 		  //встретили символ s, то рисуем 2й квадратик   
 		  if ((TileMap[i][j] == '0')) s_map.setTextureRect(IntRect(64, 0, 32, 32));//если 
 		  //встретили символ 0, то рисуем 3й квадратик 
- 
+		  if ((TileMap[i][j] == 'f')) s_map.setTextureRect(IntRect(96, 0, 32, 32));//цветок  
+		  if ((TileMap[i][j] == 'h')) s_map.setTextureRect(IntRect(128, 0, 32, 32));//сердце 
   s_map.setPosition(j * 32, i * 32);//раскладываем квадратики в карту. 
  
   window.draw(s_map);//рисуем квадратики на экран 
 	  }
 
-	  std::ostringstream playerScoreString;  // объявили переменную  
-	  playerScoreString << p.playerScore;//занесли в нее число очков, то есть формируем строку  
-	  text.setString("Собрано камней:" + playerScoreString.str());//задаем строку тексту и       // вызываем сформированную выше строку методом .str() 
-	  text.setPosition(50, 50);//задаем позицию текста, отступая от центра камеры  
-	  window.draw(text);//рисую этот текст
+	  std::ostringstream playerHealthString, gameTimeString;//объявили переменную здоровья и времени 
+	  playerHealthString << p.health; gameTimeString << gameTime;//формируем строку 
+	  text.setString("Здоровье: " + playerHealthString.str() + "\nВремя игры: " + gameTimeString.str());//задаем строку тексту 
+	  text.setPosition(50, 50);//задаем позицию текста 
+	  window.draw(text);//рисуем этот текст
 
   window.draw(p.sprite);//рисуем объект   
   window.display(); //Показываем объект на экране 
--- game/game/map.h
+++ game/game/map.h
@@ -1,21 +1,39 @@
 #include <SFML\Graphics.hpp> 
-const int HEIGHT_MAP = 15;//размер карты по высоте const 
-int WIDTH_MAP = 20;//размер карты по ширине  
+const int HEIGHT_MAP = 15;//размер карты высота const 
+int WIDTH_MAP = 20;//размер карты ширина  
  
 std::string TileMap[HEIGHT_MAP] = { 
 	"00000000000000000000", 
-	"0                  0",
 	"0                  0", 
-	"0                  0",  
-	"0     s            0", 
+	"0   s  s       f   0", 
 	"0                  0", 
+	"0     s            0",
+	"0             h    0",
+	"0  h               0", 
+	"0       f          0",
+	"0 s                0",
+	"0          h       0", 
+	"0      s           0",
 	"0                  0", 
-	"0          s       0",  
+	"0  h           f   0", 
 	"0                  0", 
-	"0                  0",  
-	"0          s       0",  
-	"0                  0", 
-	"0     s            0", 
-	"0                  0", 
-	"00000000000000000000", 
-}; 
+	"00000000000000000000",
+};
+
+void randomMapGenerate(){//рандомно расставл€ем камни 
+ 
+	int randomElementX = 0;//переменна€ дл€ хранени€ случайного элемента по горизонтали 
+	int randomElementY = 0;//переменна€ дл€ хранени€ случайного элемента по вертикали 
+	srand(time(0));//»нициализаци€ генератора случайных чисел  
+	int countStone = 3;//количество камней 
+	 while (countStone>0){  
+		randomElementX = 1 + rand() % (WIDTH_MAP - 1);//псевдослучайное значение по УxФ от 1 до  //ширина карты-1. ќграничение введено чтобы не получать числа бордюра карты  
+		randomElementY = 1 + rand() % (HEIGHT_MAP - 1);//по УyФ 
+ 
+ if (TileMap[randomElementY][randomElementX] == ' ')   {
+	 //если встретили символ пробел,    
+	 TileMap[randomElementY][randomElementX] = 's';  //то ставим туда камень.    
+	 countStone--;   
+		}  
+	} 
+};
